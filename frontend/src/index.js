eb77010a7ef19ddd943ea77766088c2746b77fda
FelixSelter <felix.selter@web.de>
2022-01-16 16:45:04 +0100

    Formating and Linting

--- frontend/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-ReactDOM.render(
-    <React.StrictMode>
-        <p>abc</p>
-    </React.StrictMode>,
-    document.getElementById('root')
-);
---
+++ frontend/src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {} from 'react-bootstrap';
+
+ReactDOM.render(<p>abc</p>, document.getElementById('root'));
